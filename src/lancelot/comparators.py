2012f3fc6d386e9a45f99e7e6df34856f663cbea
tim <tim@lightning>
2009-02-14 19:59:47 +0000

    More comparators added

--- src/lancelot/comparators.py
+++ src/lancelot/comparators.py
@@ -1,10 +1,12 @@
 '''
 Functionality for comparing two object or class / type instances
-to determine if they are "equal", with more room for semantic interpretation
-than provided by builtins "==" and "is".
+to determine if they are "equivalent", with more room for semantic 
+interpretation than provided by builtins "==" and "is".
 
 Intended public interface:
- Classes: ExceptionComparator
+ Classes: ExceptionComparator, EqualsEqualsComparator, IsSameComparator,
+     LessThanComparator, GreaterThanComparator, ContainsComparator,
+     OrComparator
  Functions: -
  Variables: -
 
@@ -16,21 +18,88 @@ Copyright 2009 by the author(s). All rights reserved
 
 class Comparator:
     ''' Comparator base class. '''
-    pass
+    
+    def __init__(self, prototype):
+        ''' Provide a prototypical instance to compare others against'''
+        self._prototype = prototype
+        
+    def __eq__(self, other):
+        ''' Delegate to compare_to method '''
+        return self.compares_to(other)
+
+    def compares_to(self, other):
+        ''' Compare prototypical instance and other instance. 
+        Always returns False for this base class'''
+        return False
+
+class EqualsEqualsComparator(Comparator):
+    ''' Comparator for handling comparison using ==. '''
+        
+    def compares_to(self, other):
+        ''' True iff prototypical instance == other '''
+        return self._prototype == other
 
 #TODO: use in Raise 
 class ExceptionComparator(Comparator):
-    ''' Comparator for handling == comparison with Exception instances. '''
-    
-    def __init__(self, exception):
-        ''' Provides the prototypical instance to compare others against'''
-        self._exception = exception
+    ''' Comparator for handling comparison with Exception instances. '''
         
-    def __eq__(self, other):
+    def compares_to(self, other):
         ''' True iff type(other) == type(prototypical exception)
         and str(other) == str(prototypical exception '''
-        if type(other) != type(self._exception):
+        if type(self._prototype) != type(other):
             return False
-        if str(other) != str(self._exception):
+        if str(self._prototype) != str(other):
             return False
         return True
+
+class IsSameComparator(Comparator):
+    ''' Comparator for handling comparison using "same". '''
+        
+    def compares_to(self, other):
+        ''' True iff prototypical instance is other '''
+        return self._prototype is other
+
+class LessThanComparator(Comparator):
+    ''' Comparator for handling comparison using <. '''
+        
+    def compares_to(self, other):
+        ''' True iff prototypical instance < other '''
+        try :
+            return self._prototype < other
+        except:
+            return False
+
+class GreaterThanComparator(Comparator):
+    ''' Comparator for handling comparison using >. '''
+        
+    def compares_to(self, other):
+        ''' True iff prototypical instance > other '''
+        try :
+            return self._prototype > other
+        except:
+            return False
+
+class ContainsComparator(Comparator):
+    ''' Comparator for handling comparison using "in" / "contains". '''
+        
+    def compares_to(self, other):
+        ''' True iff other contains prototypical instance '''
+        try:
+            return self._prototype in other
+        except:
+            return False
+
+class OrComparator(Comparator):
+    ''' Comparator for chaining comparisons using "either-or". '''
+    
+    def __init__(self, prototype, either_comparator, or_comparator):
+        super().__init__(prototype)
+        self._first_comparison = either_comparator(prototype)
+        self._second_comparison = or_comparator(prototype)
+    
+    
+    def compares_to(self, other):
+        if self._first_comparison.compares_to(other):
+            return True
+        return self._second_comparison.compares_to(other)
+        
--- src/lancelot/specs/comparator_spec.py
+++ src/lancelot/specs/comparator_spec.py
@@ -1,17 +1,112 @@
 ''' Specs for core library classes / behaviours ''' 
 
 from lancelot import Spec, verifiable, verify
-from lancelot.comparators import ExceptionComparator
+from lancelot.comparators import Comparator, EqualsEqualsComparator, \
+                                 ExceptionComparator, IsSameComparator, \
+                                 LessThanComparator, GreaterThanComparator, \
+                                 ContainsComparator, OrComparator 
 
 @verifiable
-def exception_comparator_should_compare_type_and_messsage():
-    exception_comparator = ExceptionComparator(IndexError('with message'))
-    #TODO: nicer way of forcing spec to use underlying not self
-    exception_comparator_equals = exception_comparator.__eq__
-    spec = Spec(exception_comparator_equals)
-    spec.__call__(IndexError('with message')).should_be(True)
-    spec.__call__(IndexError('with different message')).should_be(False)
-    spec.__call__(ValueError('with message')).should_be(False)
- 
+def base_comparator_behaviour():
+    ''' base Comparator should always find compared objects unequivalent.
+     Base class should also delegate __eq__ to compare_to. '''
+    Spec(Comparator(1)).compares_to(1).should_be(False)
+    Spec(Comparator(2)).compares_to(2).should_be(False)
+    Spec(Comparator(3)).compares_to(int).should_be(False)
+
+    #TODO: nicer way of forcing spec to use underlying __eq__
+    base_comparator_equals = Comparator(1).__eq__
+    spec = Spec(base_comparator_equals)
+    spec.__call__(1).should_be(False)
+    spec.__call__(2).should_be(False)
+    spec.__call__(int).should_be(False)
+
+@verifiable
+def exceptioncomparator_behaviour():
+    ''' ExceptionComparator should compare type and messsage '''
+    spec = Spec(ExceptionComparator(IndexError('with message')))
+    spec.compares_to(IndexError('with message')).should_be(True)
+    spec.compares_to(IndexError('different message')).should_be(False)
+    spec.compares_to(ValueError('with message')).should_be(False)
+
+@verifiable
+def equalscomparator_behaviour():
+    ''' EqualsEqualsComparator should compare objects with == '''
+    spec = Spec(EqualsEqualsComparator(1))
+    spec.compares_to(1).should_be(True)
+    spec.compares_to(2).should_be(False)
+    
+    spec = Spec(EqualsEqualsComparator([]))
+    spec.compares_to([]).should_be(True)
+    spec.compares_to([1]).should_be(False)
+    
+@verifiable
+def issamecomparator_behaviour():
+    ''' IsSameComparator should compare objects with "same" / "is" '''
+    spec = Spec(IsSameComparator(1))
+    spec.compares_to(1).should_be(True)
+    spec.compares_to(2).should_be(False)
+    
+    spec = Spec(IsSameComparator([]))
+    spec.compares_to([]).should_be(False) 
+    
+@verifiable
+def lessthancomparator_behaviour():
+    ''' LessThanComparator should compare objects with < '''
+    spec = Spec(LessThanComparator(1))
+    spec.compares_to(1).should_be(False)
+    spec.compares_to(2).should_be(True)
+    spec.compares_to('a').should_be(False)
+    
+    spec = Spec(LessThanComparator([]))
+    spec.compares_to([]).should_be(False)
+    spec.compares_to([1]).should_be(True)
+    
+@verifiable
+def greaterthancomparator_behaviour():
+    ''' GreaterThanComparator should compare objects with > '''
+    spec = Spec(GreaterThanComparator(2))
+    spec.compares_to(1).should_be(True)
+    spec.compares_to(2).should_be(False)
+    spec.compares_to('a').should_be(False)
+    
+    spec = Spec(GreaterThanComparator([1]))
+    spec.compares_to([]).should_be(True)
+    spec.compares_to([1]).should_be(False)
+    
+@verifiable
+def contains_comparator_behaviour():
+    ''' ContainsComparator should compare objects with "in" / "contains" '''
+    spec = Spec(ContainsComparator('a'))
+    spec.compares_to(['a', 'b']).should_be(True)
+    spec.compares_to(['a']).should_be(True)
+    spec.compares_to(['b']).should_be(False)
+    spec.compares_to('abc').should_be(True)
+    spec.compares_to('def').should_be(False)
+    spec.compares_to({'a':1}).should_be(True)
+    spec.compares_to({'b':'a'}).should_be(False)
+    spec.compares_to(2).should_be(False)
+    
+    spec = Spec(ContainsComparator(1))
+    spec.compares_to([1, 2]).should_be(True)
+    spec.compares_to([1]).should_be(True)
+    spec.compares_to([2]).should_be(False)
+    spec.compares_to('12').should_be(False)
+    spec.compares_to({1:'a'}).should_be(True)
+    spec.compares_to({'b':1}).should_be(False)
+    
+@verifiable
+def or_comparator_behaviour():
+    ''' OrComparator should chain "either-or" comparisons together '''
+    spec = Spec(OrComparator(1, EqualsEqualsComparator, LessThanComparator))
+    spec.compares_to(1).should_be(True)
+    spec.compares_to(2).should_be(True)
+    spec.compares_to('a').should_be(False)
+
+    spec = Spec(OrComparator(1, EqualsEqualsComparator, GreaterThanComparator))
+    spec.compares_to(1).should_be(True)
+    spec.compares_to(2).should_be(False)
+    spec.compares_to('a').should_be(False)
+
 if __name__ == '__main__':
     verify()
